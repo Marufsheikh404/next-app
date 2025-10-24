d10d900acdb449401821436bf52a7abb95fea9af
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-25 01:12:03 +0600

    commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const page = () => {
+const AboutPage = () => {
     return (
         <div>
             <h1>this is about page</h1>
@@ -9,4 +9,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default AboutPage;
