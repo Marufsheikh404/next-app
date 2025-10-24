1cb7ce22e3f1884453e024da480113c2e56f529e
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-10-24 13:13:26 +0600

    navber is runnding

---
+++ src/app/components/Navber.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Education</li>
+                    <li>Footer</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navber;
